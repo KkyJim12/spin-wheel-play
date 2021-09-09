ac29d33869c3eb0c0a9569b95df02a156f56f3f2
phpunch <nutacp_ryho@hotmail.com>
2021-09-09 22:12:22 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Wheel } from "./components/Wheel";
+import "./styles/main.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black min-h-screen text-white grid grid-cols-2">
+      <div className="col-span-1 p-8 bg-blue-900">
+        <div className="flex flex-row">
+          <div className="">
+            <p>สวัสดี คุณโชคดร มีสุข</p>
+            <button className="border border-black text-black bg-yellow-500 rounded-full p-2 flex flex-row justify-center items-center gap-2 ">
+              <img src="/assets/exit.png" alt="exit" className="w-8" />
+              ออกจากระบบ
+            </button>
+          </div>
+          <div>
+            <button className="border-2 border-black text-black bg-yellow-500 rounded-full p-2 flex flex-row justify-center items-center gap-2 ">
+              <img src="/assets/edit.png" alt="exit" className="w-8" />
+              แก้ไขรหัสผ่าน
+            </button>
+            <button className="border-2 border-blue-300 text-black bg-white rounded-full p-2 flex flex-row justify-center items-center gap-2">
+              <img src="/assets/coin.png" alt="exit" className="w-8" />
+              กรอกโค้ด
+            </button>
+          </div>
+          <div className="text-green-200 text-center ml-auto text-3xl">
+            <p>ระยะเวลากิจกรรม</p>
+            <div className="border-blue-400 border p-4">อีก 20 วัน</div>
+          </div>
+        </div>
+        <table className="w-full">
+          <tr className="bg-yellow-200 text-black ">
+            <th>รางวัล</th>
+            <th>พอยต์</th>
+            <th></th>
+            <th>สิทธิ์</th>
+          </tr>
+          <tr>
+            <td className="h-16 p-2">
+              <div className="border-white border-4 h-full bg-green-200"></div>
+            </td>
+            <td className="h-16 flex flex-row justify-center items-center gap-2">
+              <img src="/assets/coin.png" alt="exit" className="w-12" />
+              <p className="text-yellow-500 font-bold">x 3</p>
+            </td>
+            <td className="text-center">
+              <button className="border-black bg-yellow-500 px-4 py-1 rounded">
+                แลกรางวัล
+              </button>
+            </td>
+            <td className="text-center text-yellow-500 font-bold">0/2</td>
+          </tr>
+          <tr>
+            <td className="h-16 p-2">
+              <div className="border-white border-4 h-full bg-green-200"></div>
+            </td>
+            <td className="h-16 flex flex-row justify-center items-center gap-2">
+              <img src="/assets/star.png" alt="exit" className="w-12" />
+              <p className="text-yellow-500 font-bold">x 3</p>
+            </td>
+            <td className="text-center">
+              <button className="border-black bg-yellow-500 px-4 py-1 rounded">
+                แลกรางวัล
+              </button>
+            </td>
+            <td className="text-center text-yellow-500 font-bold">0/2</td>
+          </tr>
+        </table>
+      </div>
+      <div className="col-span-1">
+        <div className="flex flex-row gap-8 p-8">
+          <div className="relative flex-1 flex-row text-center rounded-full" style={{backgroundColor: "#05FFFE"}}>
+            <img src="/assets/coin.png" alt="exit" className="absolute w-12 -left-4 -top-2" />
+            <p className="text-black text-xl">3</p>
+          </div>
+          <div className="relative flex-1 flex-row text-center rounded-full" style={{backgroundColor: "#05FFFE"}}>
+            <img src="/assets/star.png" alt="exit" className="absolute w-12 -left-4 -top-2" />
+            <p className="text-black text-xl">3</p>
+          </div>
+          <div className="relative flex-1 flex-row text-center rounded-full" style={{backgroundColor: "#05FFFE"}}>
+            <img src="/assets/wheel.png" alt="exit" className="absolute w-12 -left-4 -top-2" />
+            <p className="text-black text-xl">3</p>
+          </div>
+        </div>
+        <div className="flex justify-center items-center h-full">
+          <Wheel />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Wheel.js
@@ -0,0 +1,259 @@
+import React from "react";
+import "./Wheel.style.css";
+
+
+export class Wheel extends React.Component {
+  state = {
+    list: ["$100", "$500", "$9,999", "$1", "$60", "$1,000"],
+    // list: ["$100", "$500", "$9,999", "$1", "$60", "$1,000", "$4.44"],
+    // list: ["$100","$500","$9,999","$1","$60"],
+    radius: 75, // PIXELS
+    rotate: 0, // DEGREES
+    easeOut: 0, // SECONDS
+    angle: 5, // RADIANS
+    top: null, // INDEX
+    offset: null, // RADIANS
+    net: null, // RADIANS
+    result: null, // INDEX
+    spinning: false
+  };
+
+  componentDidMount() {
+    // generate canvas wheel on load
+    this.renderWheel();
+  }
+
+  renderWheel() {
+    // determine number/size of sectors that need to created
+    let numOptions = this.state.list.length;
+    let arcSize = (2 * Math.PI) / numOptions;
+    this.setState({
+      angle: arcSize
+    });
+
+    // get index of starting position of selector
+    this.topPosition(numOptions, arcSize);
+
+    let colors = ["white", "blue"];
+
+    // dynamically generate sectors from state list
+    let angle = 0;
+
+    for (let i = 0; i < numOptions; i++) {
+      let text = this.state.list[i];
+      this.renderBorder(i + 1, text, angle, arcSize, colors[i % 2]);
+      this.renderSector(i + 1, text, angle, arcSize, colors[i % 2]);
+      angle += arcSize;
+    }
+    this.renderCircle();
+  }
+
+  topPosition = (num, angle) => {
+    // set starting index and angle offset based on list length
+    // works upto 9 options
+    let topSpot = null;
+    let degreesOff = null;
+    if (num === 9) {
+      topSpot = 7;
+      degreesOff = Math.PI / 2 - angle * 2;
+    } else if (num === 8) {
+      topSpot = 6;
+      degreesOff = 0;
+    } else if (num <= 7 && num > 4) {
+      topSpot = num - 1;
+      degreesOff = Math.PI / 2 - angle;
+    } else if (num === 4) {
+      topSpot = num - 1;
+      degreesOff = 0;
+    } else if (num <= 3) {
+      topSpot = num;
+      degreesOff = Math.PI / 2;
+    }
+
+    this.setState({
+      top: topSpot - 1,
+      offset: degreesOff
+    });
+  };
+
+  renderSector(index, text, start, arc, color) {
+    // create canvas arc for each list element
+    let canvas = document.getElementById("wheel");
+    let ctx = canvas.getContext("2d");
+    let x = canvas.width / 2;
+    let y = canvas.height / 2;
+    let radius = this.state.radius;
+    let startAngle = start;
+    let endAngle = start + arc;
+    let angle = index * arc;
+    let baseSize = radius * 3.33;
+    let textRadius = baseSize - 150;
+
+    ctx.beginPath();
+    ctx.arc(x, y, radius, startAngle, endAngle, false);
+    ctx.lineWidth = radius * 2;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = "blue";
+
+    ctx.font = "17px Arial";
+    ctx.fillStyle = "black";
+    ctx.stroke();
+
+    ctx.save();
+    ctx.translate(
+      baseSize + Math.cos(angle - arc / 2) * textRadius,
+      baseSize + Math.sin(angle - arc / 2) * textRadius
+    );
+    ctx.rotate(angle - arc / 2 + Math.PI / 2);
+    ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
+    ctx.restore();
+  }
+  renderBorder(index, text, start, arc, color) {
+    // create canvas arc for each list element
+    let canvas = document.getElementById("wheel");
+    let ctx = canvas.getContext("2d");
+    let x = canvas.width / 2;
+    let y = canvas.height / 2;
+    let radius = this.state.radius;
+    let startAngle = start;
+    let endAngle = start + arc;
+    let halfAngle = start + arc / 2;
+    let dotX = x + radius * 2.12 * Math.cos(halfAngle);
+    let dotY = y + radius * 2.12 * Math.sin(halfAngle);
+
+    ctx.beginPath();
+    ctx.arc(x, y, radius, startAngle, endAngle, false);
+    ctx.lineWidth = radius * 2.5;
+    ctx.strokeStyle = "orange";
+    ctx.stroke();
+
+    ctx.beginPath();
+    // ctx.arc(x, y, 10, 0, Math.PI * 2, true);
+    // ctx.stroke();
+
+    ctx.arc(dotX, dotY, 5, 0, 2 * Math.PI, false);
+    ctx.fillStyle = "white";
+    ctx.fill();
+
+    ctx.restore();
+  }
+  renderCircle() {
+    // create canvas arc for each list element
+    let canvas = document.getElementById("wheel");
+    let ctx = canvas.getContext("2d");
+    let x = canvas.width / 2;
+    let y = canvas.height / 2;
+
+    ctx.beginPath();
+    var radius = 20; // Arc radius
+    var startAngle = 0; // Starting point on circle
+    var endAngle = Math.PI + (Math.PI * 2) / 2; // End point on circle
+
+    ctx.arc(x, y, radius, startAngle, endAngle, false);
+    ctx.fillStyle = "yellow";
+
+    ctx.fill();
+    ctx.restore();
+
+    ctx.beginPath();
+    ctx.font = "20px serif";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("กด!", x - 14, y + 16 / 2);
+
+    ctx.fill();
+  }
+
+  getColor() {
+    // randomly generate rbg values for wheel sectors
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    return `rgba(${r},${g},${b},0.4)`;
+  }
+
+  spin = () => {
+    // set random spin degree and ease out time
+    // set state variables to initiate animation
+    let randomSpin = Math.floor(Math.random() * 900) + 500;
+    this.setState({
+      rotate: randomSpin,
+      easeOut: 2,
+      spinning: true
+    });
+
+    // calcalute result after wheel stops spinning
+    setTimeout(() => {
+      this.getResult(randomSpin);
+    }, 2000);
+  };
+
+  getResult = (spin) => {
+    // find net rotation and add to offset angle
+    // repeat substraction of inner angle amount from total distance traversed
+    // use count as an index to find value of result from state list
+    const { angle, top, offset, list } = this.state;
+    let netRotation = ((spin % 360) * Math.PI) / 180; // RADIANS
+    let travel = netRotation + offset;
+    let count = top + 1;
+    while (travel > 0) {
+      travel = travel - angle;
+      count--;
+    }
+    let result;
+    if (count >= 0) {
+      result = count;
+    } else {
+      result = list.length + count;
+    }
+
+    // set state variable to display result
+    this.setState({
+      net: netRotation,
+      result: result
+    });
+  };
+
+  reset = () => {
+    // reset wheel and result
+    this.setState({
+      rotate: 0,
+      easeOut: 0,
+      result: null,
+      spinning: false
+    });
+  };
+
+  render() {
+    return (
+      <div className="relative">
+        <span id="selector">&#9660;</span>
+        <canvas
+          id="wheel"
+          width="500"
+          height="500"
+          style={{
+            WebkitTransform: `rotate(${this.state.rotate}deg)`,
+            WebkitTransition: `-webkit-transform ${this.state.easeOut}s ease-out`
+          }}
+        />
+
+        {this.state.spinning ? (
+          <button type="button" id="reset" onClick={this.reset}>
+            reset
+          </button>
+        ) : (
+          <button type="button" id="spin" onClick={this.spin}>
+            spin
+          </button>
+        )}
+        {/* <div class="display">
+          <span id="readout">
+            YOU WON:{"  "}
+            <span id="result">{this.state.list[this.state.result]}</span>
+          </span>
+        </div> */}
+      </div>
+    );
+  }
+}
+
